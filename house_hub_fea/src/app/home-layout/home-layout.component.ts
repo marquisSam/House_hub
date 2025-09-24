f844d60252f29b4a7d10f444db915dc8eaa47c0f
Sam <marquis.sam@gmail.com>
2025-09-24 08:58:48 -0400

    refactor(home-layout): update layout structure and styles
    - Replaced the old menu structure with a new sidebar layout using ng-zorro-antd components.
    - Updated the HTML template to include a sidebar and content area.
    - Modified SCSS to support the new layout, ensuring proper sizing and overflow handling.
    - Imported necessary ng-zorro-antd modules for layout and menu functionality.

--- house_hub_fea/src/app/home-layout/home-layout.component.ts
+++ house_hub_fea/src/app/home-layout/home-layout.component.ts
@@ -1,9 +1,19 @@
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
 
 @Component({
   selector: 'app-home-layout',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    NzLayoutModule,
+    NzIconModule,
+    NzMenuModule,
+  ],
   templateUrl: './home-layout.component.html',
   styleUrl: './home-layout.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
